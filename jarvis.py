ed57788971e1b0178583d75f1c0e1a86ef4a2dfc
Shubham Bawankar <71594948+Shubham56-droid@users.noreply.github.com>
2021-11-04 21:23:13 +0530

    Add files via upload

---
+++ jarvis.py
@@ -0,0 +1,51 @@
+import pyttsx3
+import PyAudio
+import speech_recognition as sr
+import datetime
+
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+engine.setProperty('voice',voices[1].id)
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if(hour>=0 and hour<12):
+        speak("Good Morning Sir!")
+    elif(hour>=12 and hour<18):
+        speak("Good Afternoon Sir!")
+    elif(hour>=18 and hour<21):
+        speak("Good Evening Sir")
+    else:
+        speak("Good Night Sir")
+    speak("I am jarvis your personal assistant Please tell me how may i help you.")
+
+def takeCommand():
+    #It takes microphone input from the user and returns string output
+
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing...")    
+        query = r.recognize_google(audio, language='en-in') #Using google for voice recognition.
+        print("User said:", {query})  #User query will be printed.
+
+    except Exception as e:
+        # print(e)    
+        print("Say that again please...")   #Say that again will be printed in case of improper voice 
+        return "None" #None string will be returned
+    return query
+
+if __name__ == "__main__":
+    wishMe()
+    takeCommand()
+
+     
+    
