6a3239a451dc49798ccdc090deb2c9df3520a283
Shubham Bawankar <71594948+Shubham56-droid@users.noreply.github.com>
2021-11-08 09:20:15 +0530

    Update jarvis.py

--- jarvis.py
+++ jarvis.py
@@ -1,3 +1,4 @@
+import datetime
 import pyttsx3
 import speech_recognition as sr
 import pyaudio
@@ -5,6 +6,7 @@ import wikipedia
 import os
 import webbrowser
 import pywhatkit
+import pyautogui
 
 Assistant = pyttsx3.init('sapi5')
 voices = Assistant.getProperty('voices')
@@ -43,8 +45,36 @@ def TaskExe():
         Speak("tell me the name of music")
         musicname = takeCommand()
 
-        if 'On my way' in query:
-            os.startfile('')
+        if 'on my way' in musicname:
+            Speak("Playing music sir")
+            os.startfile('E:\\my_music\\Alan Walker, Sabrina Carpenter & Farruko - On My Way.mp3')
+
+        elif 'apna har din' in musicname:
+            Speak("Playing music sir")
+            os.startfile('E:\\my_music\\Apna Har Din 128 Kbps.mp3')
+        elif 'believer' in musicname:
+            Speak("Playing music sir")
+            os.startfile('E:\\my_music\\Believer(PagalWorld).mp3')
+
+        elif 'I dont want to listen music' in query:
+            Speak("Ok sir music closed")
+            return
+
+
+    def OpenApp():
+        Speak("Ok sir, wait a second sir")
+
+        if 'vs code' in query:
+            os.startfile('"C:\\Users\\ADMIN\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"')
+        elif 'pycharm' in query:
+            os.startfile("C:\\Program Files\\JetBrains\\PyCharm Community Edition 2021.2.3\\bin\\pycharm64.exe")
+        elif 'citra' in query:
+            os.startfile('C:\\Users\\ADMIN\\AppData\\Local\\Citra\\canary-mingw\\citra-qt.exe')
+        elif 'chrome' in query:
+            os.startfile("C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe")
+        elif 'no need to open app' in query:
+            return
+
 
     while True:
         query = takeCommand()
@@ -68,13 +98,28 @@ def TaskExe():
             Speak(results)
 
         elif 'open youtube' in query:
-            
+            Speak("Opening youtube sir...")
+            youtube = "https://www.youtube.com/"
+            webbrowser.open(youtube)
+            Speak("Done Sir")
+
+        elif 'time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")
+            Speak(f"Sir the time is, {strTime}")
+
 
         elif 'search in google' in query:
             Speak("Ok sir, This is what i found for your search in chrome")
             query = query.replace("search in google", "")
             pywhatkit.search(query)
             Speak("Done Sir")
+
+        elif 'tell me about' in query:
+            Speak("Yes sir, Searching about it sir...")
+            query = query.replace("tell me about", "")
+            result = wikipedia.summary(query, sentences=3)
+            Speak("According to my searches")
+            Speak(result)
 
         elif 'open website' in query:
             Speak("Ok sir, launching...")
@@ -91,9 +136,17 @@ def TaskExe():
             webbrowser.open(web3)
             Speak("Done Sir")
 
+        elif 'screenshot' or 'Screenshot' in query:
+            Speak("Ok sir, what should be the name of the file")
+            path = takeCommand()
+            pathname = path + ".png"
+            path1 = "C:\\Users\\ADMIN\\Pictures\\Screenshots\\"+pathname
+            ss = pyautogui.screenshot()
+            ss.save(path1)
+            os.startfile("C:\\Users\\ADMIN\\Pictures\\Screenshots")
+            Speak("Here is your screenshot sir")
 
-
-        elif 'shutdow' in query:
+        elif 'shutdown' in query:
             Speak("Jarvis shutting down sir")
             break
 
